a0594eea48df6a2c6b620632503cc5712c7092c7
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-23 19:19:33 +0530

    set up multer middleware

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { addProduct,listProduct,removeProduct,singleProduct } from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
 
 const productRouter = express.Router();
 
-productRouter.post('/add',addProduct);
+productRouter.post('/add',upload.fields([{name:"image1",maxCount:1},{name:"image2",maxCount:1},{name:"image3",maxCount:1},{name:"image4",maxCount:1}]),addProduct);
 productRouter.post('/remove',removeProduct);
 productRouter.post('/single',singleProduct);
 productRouter.post('/list',listProduct);
