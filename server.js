ca698a4cd0cb56dc11708141d8c04006ab9296c9
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-23 14:22:02 +0530

    create our first server

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config'
+
+// App config 
+const app = express();
+const port = process.env.PORT || 4000
+
+// middlewares 
+app.use(express.json());
+app.use(cors());
+
+// api endpoint 
+
+app.get("/",(req,res)=>{
+    res.send("Hii This is our first server")
+})
+
+app.listen(port,()=>{
+    console.log(`server running port no. ${port}`);
+    
+})
