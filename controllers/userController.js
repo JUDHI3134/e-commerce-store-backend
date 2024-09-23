ef82cbe70f454bffdf8995d94e7af2103e767e51
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-23 15:07:41 +0530

    create route setup

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+
+
+//Route for User Login
+
+const loginUser = async (req,res) =>{
+
+}
+
+
+//Route for user Register
+
+const registerUser = async (req,res) =>{
+
+}
+
+
+//route for admin Login
+
+const adminLogin = async (req,res) =>{
+
+}
+
+
+
+export {loginUser , registerUser, adminLogin}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 // App config 
 const app = express();
@@ -15,6 +16,8 @@ connectDB();
 connectCloudinary()
 
 // api endpoint 
+
+app.use("/api/user",userRouter)
 
 app.get("/",(req,res)=>{
     res.send("Hii This is our first server")
