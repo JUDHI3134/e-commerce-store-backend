0d166ddb3f71b9360a1f9f915c431c0d40d805c3
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-23 18:57:34 +0530

    create login api end point

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,25 @@ const createToken = (id) =>{
 //Route for User Login
 
 const loginUser = async (req,res) =>{
+    try {
+        const {email,password} = req.body;
 
+        const user = await userModel.findOne({email});
+        if(!user){
+            res.json({success:false,message:"User does not exist."})
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(isMatch){
+            const token = createToken(user._id);
+            res.json({success:true,token})
+        }else{
+            res.json({success:false,message:"Invalid Credentials"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 }
 
 
