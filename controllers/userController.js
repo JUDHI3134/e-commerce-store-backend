e7aedc9bda97408f982072c09748c141938be703
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-24 08:34:50 +0530

    create admin api end point

--- controllers/userController.js
+++ controllers/userController.js
@@ -82,7 +82,18 @@ const registerUser = async (req,res) =>{
 //route for admin Login
 
 const adminLogin = async (req,res) =>{
-
+   try {
+    const {email, password} = req.body;
+    if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+        const token = jwt.sign(email+password,process.env.JWT_SECRET);
+        res.json({success:true, token})
+    }else{
+        res.json({success:false, message: "Invalid Credentials"})
+    }
+   } catch (error) {
+    console.log(error);
+    res.json({success:false,message:error.message})
+   }
 }
 
 
