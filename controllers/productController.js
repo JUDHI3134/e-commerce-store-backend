d0be5fc94535b83bb05b33f3b59e7aa74aa2e6be
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-24 08:01:36 +0530

    create add product api endpoint

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,46 @@
-
+import {v2 as cloudinary} from "cloudinary"
+import productModel from "../models/ProductModel.js";
 
 //function for add product
 
 const addProduct = async (req,res)=>{
+    try {
+        const {name,description,price,category,subCategory,sizes,bestseller} = req.body;
     
+        const image1 =req.files.image1 && req.files.image1[0];
+        const image2 =req.files.image2 && req.files.image2[0];
+        const image3 =req.files.image3 && req.files.image3[0];
+        const image4 =req.files.image4 && req.files.image4[0];
+
+        const images = [image1,image2,image3,image4].filter((item)=> item !== undefined)
+
+        let imageUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path,{resource_type: "image"});
+                return result.secure_url;
+            })
+        )
+    
+        const productData = {
+            name,
+            description,
+            price: Number(price),
+            category,
+            subCategory,
+            bestseller: bestseller === "true" ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imageUrl,
+            date: Date.now()
+        }
+        console.log(productData);
+        
+        const product = new productModel(productData)
+        await product.save();
+        
+        res.json({success: true, message: "Product Added"})
+    } catch (error) {
+        res.json({success:false,message:error.message})
+    }
 }
 
 
