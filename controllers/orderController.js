99701c56752e8c622461cb2720dda3198dc1534b
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-25 15:15:37 +0530

    create api endpoint for stripe payment gateway

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,13 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/UserModel.js";
+import Stripe from "stripe";
 
+//global variables
+const currency = "inr"
+const deliveryCharge = 10
+
+//gateway initialized
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing order using COD
 const placeOrder = async (req,res) =>{
@@ -33,7 +40,60 @@ const placeOrder = async (req,res) =>{
 
 //placing order using Stripe method
 const placeOrderStripe = async (req,res) =>{
+    try {
+        const {userId, amount, items, address} = req.body;
+        const {origin} = req.headers;
 
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod: "Stripe",
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save();
+
+        const line_items = items.map((item)=>({
+            price_data:{
+                currency: currency,
+                product_data:{
+                    name: item.name
+                },
+                unit_amount: item.price * 100
+            },
+            quantity: item.quantity
+        }))
+
+        line_items.push({
+            price_data:{
+                currency: currency,
+                product_data:{
+                    name: "Delivery Charges"
+                },
+                unit_amount: deliveryCharge * 100
+            },
+            quantity: 1
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: 'payment',
+        })
+
+        res.json({success:true, session_url : session_url})
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 }
 
 
