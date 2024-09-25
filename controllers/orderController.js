400bc3e9c94033d2352650bcec666cd20655ad6b
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-25 12:53:42 +0530

    create user orders api end point

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -53,7 +53,17 @@ const allOrders = async (req,res) =>{
 
 //User order data for frontend
 const userOrders = async (req,res) =>{
+   try {
+    
+    const {userId} = req.body;
+    const orders = await orderModel.find({userId})
 
+    res.json({success: true,orders})
+
+   } catch (error) {
+    console.log(error);
+    res.json({success: false, message: error.message})
+   }
 }
 
 
