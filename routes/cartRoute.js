fa30fbfee48237288d38a024f12811bb2393201e
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-25 10:11:35 +0530

    create authUser for cart functionality

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { addToCart, updateCart, getUserCart } from "../controllers/cartController.js"
+import authUser from "../middleware/auth.js";
 
 const cartRouter = express.Router();
 
-cartRouter.post('/get',getUserCart)
-cartRouter.post('/add',addToCart)
-cartRouter.post('/update',updateCart)
+cartRouter.post('/get', authUser ,getUserCart)
+cartRouter.post('/add', authUser ,addToCart)
+cartRouter.post('/update', authUser ,updateCart)
 
 export default cartRouter;
