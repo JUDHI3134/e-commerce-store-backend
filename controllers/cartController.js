1d6bafbd3122ac1aa7905884a5e41b4d359d5056
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-25 10:23:02 +0530

    create logic for add to cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,8 +1,31 @@
+import userModel from "../models/UserModel.js";
 
 
 //add product to user cart
 const addToCart = async (req,res) =>{
-    
+    try {
+        const {userId, itemId, size} = req.body;
+        const userData = await userModel.findById(userId)
+        let cartData = await userData.cartData
+
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] += 1
+            }else{
+                cartData[itemId][size] = 1
+            }
+        }else{
+            cartData[itemId] = {}
+            cartData[itemId][size] = 1
+        }
+
+        await userModel.findByIdAndUpdate(userId, {cartData})
+        res.json({success: true, message: "Added To Cart"})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 }
 
 
