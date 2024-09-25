6f0c865b06fefe7030548548656fe35ddd908208
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-25 14:43:42 +0530

    create order status update api endpoint

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -77,7 +77,14 @@ const userOrders = async (req,res) =>{
 
 //Update order status from Admin
 const updateStatus = async (req,res) =>{
-
+    try {
+       const {orderId, status} = req.body;
+       await orderModel.findByIdAndUpdate(orderId,{ status }) 
+       res.json({success: true, message: "Status Updated"})
+    } catch (error) {
+        console.log(error);
+    res.json({success: false, message: error.message})
+    }
 }
 
 export {updateStatus,userOrders,allOrders,placeOrder,placeOrderRazorpay,placeOrderStripe}
